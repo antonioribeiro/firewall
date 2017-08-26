9d390281666ca194157b8c98705f477b86e55ba0
Just Playin <jp@justplayingames.com>
2017-08-26 10:05:20 -0500

    Minor syntax fix
    Fix for https://github.com/antonioribeiro/firewall/issues/77

--- src/Firewall.php
+++ src/Firewall.php
@@ -581,7 +581,8 @@ class Firewall
      */
     public function updateGeoIp()
     {
-        $success = ($updater = new GeoIpUpdater())->updateGeoIpFiles($this->config->get('geoip_database_path'));
+        $updater = new GeoIpUpdater();
+        $success = $updater->updateGeoIpFiles($this->config->get('geoip_database_path'));
 
         $this->messages = $updater->getMessages();
 
